2196a9c6341d1c9656d9f1772644d6a023e8c1ad
RaniaAmmari <106685416+RaniaAmmari@users.noreply.github.com>
2022-06-14 16:38:04 +0100

    Update index.php

--- Activité 1.5.1/index.php
+++ Activité 1.5.1/index.php
@@ -1,3 +1,10 @@
+<?php
+     session_start();
+
+     if(!isset($_SESSION['logged_user'])){
+         header('Location:'.$rootUrl.'login.php');
+     }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
